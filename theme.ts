068e96fd419be06726a5c93fe74a4436156674eb
peashooter45 <balvinpatrick@yahoo.com>
2023-08-09 23:52:18 -0400

    initial commit

---
+++ theme.ts
@@ -0,0 +1,37 @@
+import { buildLegacyTheme } from "sanity";
+
+const props = {
+  "--my-white": "#fff",
+  "--my-black": "#1a1a1a",
+  "--alvs-theme": "#F7AB0A",
+  "--my-red": "#db4437",
+  "--my-yellow": "#f4b400",
+  "--my-green": "#0f9d58",
+};
+
+export const myTheme = buildLegacyTheme({
+  /* Base theme colors */
+  "--black": props["--my-black"],
+  "--white": props["--my-white"],
+  "--gray": "#666",
+  "--gray-base": "#666",
+  "--component-bg": props["--my-black"],
+  "--component-text-color": props["--my-white"],
+  /* Brand */
+  "--brand-primary": props["--alvs-theme"],
+  // Default button
+  "--default-button-color": "#666",
+  "--default-button-primary-color": props["--alvs-theme"],
+  "--default-button-success-color": props["--my-green"],
+  "--default-button-warning-color": props["--my-yellow"],
+  "--default-button-danger-color": props["--my-red"],
+  /* State */
+  "--state-info-color": props["--alvs-theme"],
+  "--state-success-color": props["--my-green"],
+  "--state-warning-color": props["--my-yellow"],
+  "--state-danger-color": props["--my-red"],
+  /* Navbar */
+  "--main-navigation-color": props["--my-black"],
+  "--main-navigation-color--inverted": props["--my-white"],
+  "--focus-color": props["--alvs-theme"],
+});
